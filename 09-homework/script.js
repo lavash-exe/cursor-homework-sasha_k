dd8b3f1ae7490a89cdbaed296a9c49f8c13aad8e
lavash <alexkosar376@gmail.com>
2020-08-15 19:23:49 +0300

    added hw 09

---
+++ 09-homework/script.js
@@ -0,0 +1,24 @@
+
+setInterval(function () {
+    $color = generateColor();
+    $(`#${randomNumber()}`).css('background-color', $color)
+}, 1000);
+
+function generateBlocks() {
+    const mainDiv = document.getElementById('square-block')
+    for (let i = 1; i <= 25; i++){
+        let blockDiv = document.createElement('div')
+        blockDiv.id = `${i}`
+        blockDiv.style.backgroundColor = generateColor()
+        mainDiv.appendChild(blockDiv)
+    }
+
+}
+generateBlocks()
+function generateColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function randomNumber() {
+    return Math.floor(Math.random() * 25) + 1;
+}
