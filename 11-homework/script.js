385ed654b2e5f57b227a6811f2c5b09993dedbda
lavash <alexkosar376@gmail.com>
2020-08-22 20:16:11 +0300

    added 11 hw

---
+++ 11-homework/script.js
@@ -0,0 +1,19 @@
+function getRandomChinese(length) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let sign = Date.now().toString().slice(-5)
+            let chineseArr = []
+            for (let i = 0; i < length; i++) {
+                sign += i;
+                chineseArr.push(String.fromCharCode(sign))
+            }
+            if (length <= 20) {
+                resolve(chineseArr.join(''))
+            } else {
+                reject(new Error('Надто великі числа валиш, краще постав 5'))
+            }
+        }, 50 * length)
+    })
+}
+
+console.log('getRandomChinese: ', getRandomChinese(21));
